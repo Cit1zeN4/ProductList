e1af065fb805a06ac21ea9ab4e8b15740ebf6f34
Max <maximrimkus@yandex.by>
2024-02-19 23:02:17 +0300

    Added ProductList.Application DI

---
+++ ProductList.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using ProductList.Application.Common.Mappings;
+using ProductList.Application.Interfaces;
+
+namespace ProductList.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(config =>
+        {
+            config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+            config.AddProfile(new AssemblyMappingProfile(typeof(IProductListDbContext).Assembly));
+        });
+        
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        return services;
+    }
+}
