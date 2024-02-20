b55525a50d345a0a04731635c541de3d1a83c033
Max <maximrimkus@yandex.by>
2024-02-20 13:36:57 +0300

    Added UserInfoServiceMock.cs

---
+++ ProductList.Test/Common/UserInfoServiceMock.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using ProductList.Application.Common;
+using ProductList.Application.Interfaces;
+
+namespace ProductList.Test.Common;
+
+public static class UserInfoServiceMock
+{
+    public static IUserInfoService GetAdminMock()
+    {
+        return Mock.Of<IUserInfoService>(x => x.Role == Roles.Admin);
+    }
+    
+    public static IUserInfoService GetModeratorMock()
+    {
+        return Mock.Of<IUserInfoService>(x => x.Role == Roles.Moderator);
+    }
+    
+    public static IUserInfoService GetUserMock()
+    {
+        return Mock.Of<IUserInfoService>(x => x.Role == Roles.User);
+    }
+}
