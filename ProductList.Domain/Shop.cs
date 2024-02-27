2c123a4c33464fc2df13df8a4a8262a737c880f3
Max <maximrimkus@yandex.by>
2024-02-27 20:14:50 +0300

    Update Shop.cs

--- ProductList.Domain/Shop.cs
+++ ProductList.Domain/Shop.cs
@@ -5,4 +5,5 @@ public class Shop
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Address { get; set; }
+    public DateTimeOffset CreateAt { get; set; } = DateTimeOffset.UtcNow;
 }
