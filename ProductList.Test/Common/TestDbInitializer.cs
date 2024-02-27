fa88d99b8d72602e78ebd338ac284f14097b3ef1
Max <maximrimkus@yandex.by>
2024-02-27 19:57:59 +0300

    Added GetShopQueryTest.cs

--- ProductList.Test/Common/TestDbInitializer.cs
+++ ProductList.Test/Common/TestDbInitializer.cs
@@ -8,6 +8,7 @@ public static class TestDbInitializer
     {
         public static Guid ShopToDeleteId = Guid.NewGuid();
         public static Guid ShopToUpdateId = Guid.NewGuid();
+        public static Guid ShopToGetId = Guid.NewGuid();
     
         public static void InitShop(IProductListDbContext context)
         {
@@ -23,6 +24,18 @@ public static class TestDbInitializer
                     Id = ShopToUpdateId,
                     Name = "Test shop2",
                     Address = "Test shop2 address"
+                },
+                new Domain.Shop()
+                {
+                    Id = ShopToGetId,
+                    Name = "Unique shop",
+                    Address = "Night City 52/1"
+                },
+                new Domain.Shop()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Shop",
+                    Address = "Night City 52/2"
                 }
             );
         }
---
+++ ProductList.Test/Shop/Queries/GetShopQueryTest.cs
@@ -0,0 +1,53 @@
+﻿using ShopDb = ProductList.Test.Common.TestDbInitializer.ShopInitializer;
+
+using ProductList.Application.Interfaces;
+using ProductList.Application.Logic.Shop.Queries.GetShop;
+using ProductList.Test.Common;
+
+namespace ProductList.Test.Shop.Queries;
+
+[Collection("InitDbCollection")]
+public class GetShopQueryTest(InitDbFixture fixture)
+{
+    private readonly IProductListDbContext _context = fixture.Context;
+
+    [Fact]
+    public async Task GetShopQuery_Success()
+    {
+        // Arrange
+        var query = new GetShopQuery
+        {
+            Id = ShopDb.ShopToGetId
+        };
+
+        var validator = new GetShopQueryValidator();
+        var handler = new GetShopQueryHandler(_context);
+        
+        // Act
+        var validation = await validator.ValidateAsync(query);
+        var result = await handler.Handle(query, CancellationToken.None);
+        
+        // Assert
+        validation.IsValid.ShouldBeTrue();
+        result.ShouldNotBeNull();
+        result.Id.ShouldBe(ShopDb.ShopToGetId);
+    }
+
+    [Fact]
+    public async Task GetShopQuery_ValidationError()
+    {
+        // Arrange
+        var query = new GetShopQuery
+        {
+            Id = Guid.Empty
+        };
+
+        var validator = new GetShopQueryValidator();
+
+        // Act
+        var validation = await validator.ValidateAsync(query);
+        
+        // Assert
+        validation.IsValid.ShouldBeFalse();
+    }
+}
