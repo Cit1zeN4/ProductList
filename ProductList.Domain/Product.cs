ba39b5bc8876cd807eea10ed91036bf88288118e
Max <maximrimkus@yandex.by>
2024-02-28 13:35:09 +0300

    Updated Product.cs

--- ProductList.Domain/Product.cs
+++ ProductList.Domain/Product.cs
@@ -5,7 +5,7 @@ public class Product
     public Guid Id { get; set; }
     public string Barcode { get; set; }
     public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public virtual List<Image> Images { get; set; }
     public virtual List<Shop> Shops { get; set; }
     public virtual List<Purchase> Carts { get; set; } 
