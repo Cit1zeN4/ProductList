603f2c87ce14d7d2a2c67ff359644095e58ca41f
Max <maximrimkus@yandex.by>
2024-03-01 23:53:35 +0300

    Updated GetProductQueryValidator.cs

--- ProductList.Application/Logic/Product/Query/GetProduct/GetProductQueryValidator.cs
+++ ProductList.Application/Logic/Product/Query/GetProduct/GetProductQueryValidator.cs
@@ -6,26 +6,18 @@ public class GetProductQueryValidator : AbstractValidator<GetProductQuery>
 {
     public GetProductQueryValidator()
     {
-        When(x => x.Id.HasValue, () =>
-        {
-            RuleFor(y => y.Barcode).Null();
-            RuleFor(y => y.Id).NotNull().NotEqual(Guid.Empty);
-        });
+        When(x => x.Id.HasValue && string.IsNullOrEmpty(x.Barcode), () =>
+            {
+                RuleFor(y => y.Barcode).Null();
+                RuleFor(y => y.Id).NotNull().NotEqual(Guid.Empty);
+            })
+            .Otherwise(() =>
+            {
+                RuleFor(y => y.Id).Null();
+                RuleFor(y => y.Barcode).NotNull().NotEmpty().MinimumLength(1).MaximumLength(50);
+            });
 
-        When(x => !x.Id.HasValue, () =>
-        {
-            RuleFor(y => y.Barcode).NotNull().NotEmpty().MinimumLength(1).MaximumLength(50);
-        });
-
-        When(x => !string.IsNullOrEmpty(x.Barcode), () =>
-        {
-            RuleFor(y => y.Id).Null();
-            RuleFor(y => y.Barcode).NotNull().NotEmpty().MinimumLength(1).MaximumLength(50);
-        });
-
-        When(x => !string.IsNullOrEmpty(x.Barcode), () =>
-        {
-            RuleFor(y => y.Id).NotNull().NotEqual(Guid.Empty);
-        });
+        RuleFor(y => y.Id).NotNull().NotEqual(Guid.Empty);
+        RuleFor(y => y.Barcode).NotNull().NotEmpty().MinimumLength(1).MaximumLength(50);
     }
 }
